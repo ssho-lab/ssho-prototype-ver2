cb83d0f4a7deee0f7116c7ba229c791206254746
Jin Young Park <jy2045@sogang.ac.kr>
2020-07-13 21:59:09 +0900

    Bubble Sample

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import * as React from "react";
-import { View} from "react-native";
-import RootNavigator from "./navigation/RootNavigator";
-import { Provider } from "mobx-react";
-import RootStore from "./src/stores";
+import * as React from 'react';
+import {View} from 'react-native';
+import RootNavigator from './navigation/RootNavigator';
+import {Provider} from 'mobx-react';
+import RootStore from './src/stores';
 
 const root = new RootStore();
 
 const App = () => {
   return (
     <Provider {...root}>
-      <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
         <RootNavigator />
       </View>
     </Provider>
