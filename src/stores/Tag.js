01f1886c9e763976c17e199a46a7c9699e3a7ee2
yoon-chaejin <kevin3st@naver.com>
2020-08-17 21:40:45 +0900

    ios 오류 해결 && 태그 API 연동

--- src/stores/Tag.js
+++ src/stores/Tag.js
@@ -1,6 +1,10 @@
 import {observable, action} from 'mobx';
+import tagModel from '../models/TagModel';
+import TagRepository from '../repos/TagRepository';
 
-class TagStore {
+const tagRepository = new TagRepository();
+
+class tagStore {
   constructor(root) {
     this.root = root;
   }
@@ -9,9 +13,17 @@ class TagStore {
   @observable tagList = [];
 
   // Todo 2. API 연결해서 Tag List 받아오기
-  @action getItem() {
-    return this.tagList;
+  @action 
+  async getItem() {
+    let response;
+    try {
+      response = await tagRepository.getTagList();
+    } catch (error) {
+      console.log(error);
+    }
+    const data = response.data;
+    this.tagList = data.map(tag => new tagModel(tag));
   }
 }
 
-export default TagStore;
+export default tagStore;
