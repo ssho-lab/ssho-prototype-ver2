8887b73a75eac6c106b71bb89ae761948747493c
Jin Young Park <jy2045@sogang.ac.kr>
2020-10-07 02:22:10 +0900

    [feat] 임의 token 설정

--- src/repos/ShoppingBagRepository.js
+++ src/repos/ShoppingBagRepository.js
@@ -3,7 +3,8 @@ import temp from '../temp';
 
 class ShoppingBagRepository {
   URL = 'http://13.124.59.2:8081';
-  token = 'token';
+  token =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzc2hvIiwidXNlcl9pZHgiOjE4LCJleHAiOjE2MDQ2NzM1Nzh9.cFY430MRfKvyAwOz3XvHwvIl8OsOdQFKufmSXO7uMbE';
 
   constructor(url) {
     this.URL = url || this.URL;
