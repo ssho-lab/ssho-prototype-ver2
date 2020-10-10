9ec71aee0cc736433462af195253493b6e9169da
Jin Young Park <jy2045@sogang.ac.kr>
2020-10-10 21:25:35 +0900

    [fix] 쇼핑백 조회 API 오류 해결
    * URL이 잘못들어가있었다...
    * 추가로 AndroidManifest.xml에 useCleartextTraffic = true 옵션 추가

--- src/repos/ShoppingBagRepository.js
+++ src/repos/ShoppingBagRepository.js
@@ -2,18 +2,19 @@ import axios from 'axios';
 import temp from '../temp';
 
 class ShoppingBagRepository {
-  URL = 'http://13.124.59.2:8081';
+  URL = 'http://13.124.59.2';
   token =
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzc2hvIiwidXNlcl9pZHgiOjE4LCJleHAiOjE2MDQ2NzM1Nzh9.cFY430MRfKvyAwOz3XvHwvIl8OsOdQFKufmSXO7uMbE';
-
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzc2hvIiwidXNlcl9pZHgiOjE4LCJleHAiOjE2MDUwMTA4MzV9.vwk36zLdeaJWTNgAamE6cHYmpudrEtETX_bhgbVwTWc';
   constructor(url) {
     this.URL = url || this.URL;
     this.token = this.token;
   }
 
   fetchItemList() {
-    return axios.get('/item/shopping-bag', {
-      headers: {Authorization: this.token},
+    return axios.get(this.URL + ':8081/item/shopping-bag', {
+      headers: {
+        Authorization: this.token,
+      },
     });
   }
 }
